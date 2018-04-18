43495116eb350daf2edd344ec71d7293d46a8b69
Michael Bestas <mkbestas@lineageos.org>
2018-04-18 23:31:23 +0300

    sdm660-common: Add init.recovery.qcom.rc
    * Copied from wahoo init.recovery.hardware.rc,
      fixes partition mounting and ADB in recovery
    
    Change-Id: I8a0136dd08c0df0ffb08bec8f35bffd38f5d44bb

--- device.mk
+++ device.mk
@@ -330,6 +330,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
     fstab.qcom
