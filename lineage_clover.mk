bdf471380002d800468ac753a853b567b60f5739
celeron633 <dengxionghui@hotmail.com>
2026-03-12 23:13:58 -0700

    clover: disable adb authorize for now
    Change-Id: I182b2ad3e27fed0640b88bb992a55ae0fb95c167

--- lineage_clover.mk
+++ lineage_clover.mk
@@ -22,6 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit from clover device
 $(call inherit-product, device/xiaomi/clover/device.mk)
 
+WITH_ADB_INSECURE := true
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, vendor/lineage/config/telephony.mk)
